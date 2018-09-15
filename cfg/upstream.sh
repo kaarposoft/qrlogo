18b22a05d66ede13e91b22bd02cdb6784484f262
Henrik <henrik@kaarposoft.dk>
2018-09-15 16:10:28 +0200

    update to wasm-bindgen 0.2.21 and js-sys 0.2.6

--- cfg/upstream.sh
+++ cfg/upstream.sh
@@ -10,7 +10,7 @@ printf "\n>>> >>> RUST_TOOLCHAIN: %s\n" "${RUST_TOOLCHAIN}"
 # **** **** MAKE SURE THAT THE VERSION IS THE SAME AS IN Cargo.toml !!!
 # **** ****
 # ============================================================
-WASM_BINDGEN="0.2.19"
+WASM_BINDGEN="0.2.21"
 printf ">>> >>> WASM_BINDGEN: %s\n" "${WASM_BINDGEN}"
 WASM_BINDGEN_INSTALL="--version=${WASM_BINDGEN}"
 WASM_BINDGEN_VERSION_STRING="wasm-bindgen-cli v${WASM_BINDGEN}"
